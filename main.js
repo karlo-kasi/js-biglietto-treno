b4f3195c4aaa7248769af0652a14f3dafff23e12
karlo-kasi <karlokasi1995@gmail.com>
2025-01-09 17:10:53 +0100

    Add: let + const

---
+++ main.js
@@ -0,0 +1,27 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+/* 
+1 - Inizializzare / dichiarare le variabili e costanti
+2 - check up se se valori sono numeri 
+    utilizzare un metodoto per trasformare le stringhe in numeri parsetInt
+*/
+
+let km = parseInt ( prompt("Quanti chilometri devi fare?", Math.floor(Math.random() * 100) + 20));
+let età = parseInt ( prompt("Quanti anni hai?",  Math.floor(Math.random() * 100) + 10));
+const kmInEuro = 0.21;
+
+// 2 - check up se i valori sono numeri
+if( isNaN(età)) {
+    //se true
+    console.log (`${età} è un numero`)
+} else {
+    //se foulse
+    console.log (`${età} non è un numero`)
+    età = prompt("Quanti anni hai?")
+}
