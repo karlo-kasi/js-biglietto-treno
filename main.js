da91b588dc64d34ad2189dea6f353b7030c2d539
karlo-kasi <karlokasi1995@gmail.com>
2025-01-09 19:49:47 +0100

    Add: price discount

--- main.js
+++ main.js
@@ -10,17 +10,23 @@
 1 - Inizializzare / dichiarare le variabili e costanti
 2 - check up se se valori sono numeri 
     utilizzare un metodoto per trasformare le stringhe in numeri parsetInt
-      2.1 - verifica che se sotto i 18 - applicare uno sconto del 20%, sennò niente sconto
-      2.2 - verificare che se sopra i 65 anni - applicare uno sconto del 40%, sennò niente sconto 
+      2.1 - definire il prezzo del biglietto con moltiplicazione
+      2.2 - verifica che se sotto i 18 - applicare uno sconto del 20%, sennò niente sconto
+      2.3 - verificare che se sopra i 65 anni - applicare uno sconto del 40%, sennò niente sconto 
 3 - Stampare il risultato su console 
 */
 
 // 1 - Dichiarare i Valori
 let km = prompt("Quanti chilometri devi fare?", Math.floor(Math.random() * 100) + 20);
 let età = prompt("Quanti anni hai?",  Math.floor(Math.random() * 100) + 10);
-const kmInEuro = 0.21;
+const euro = 0.21;
+const sconto1 = 20;
+const sconto2 = 40;
 
-console.log (parseInt (km), parseInt (età))
+km = parseFloat(km);
+età = parseInt(età);
+
+console.log ((km), (età))
 
 // 2 - Controlla se i valori sono numeri
 if (!isNaN(età) && !isNaN(km)) {
@@ -30,4 +36,24 @@ if (!isNaN(età) && !isNaN(km)) {
     //se foulse
     console.log (`${età} e ${km} NON sono un numero`)
 }
+
+// 2.1 Definire il prezzo del biglietto in base ai km 
+
+let prezzoBiglietto = (km * euro);
+
+console.log (`${prezzoBiglietto.toFixed(2)} € è il prezzo del tuo biglietto`);
+
+//2.2 2.3 - verifica che se sotto i 18 - applicare uno sconto del 20%, sennò niente sconto
+
+let prezzoBigliettoSconto1 = (prezzoBiglietto * 0.8)
+let prezzoBigliettoSconto2 = (prezzoBiglietto * 0.6)
+
+console.log (prezzoBigliettoSconto1, prezzoBigliettoSconto2 )
+
+if (età < 18) {
+    console.log(`Ha diritto allo sconto del ${sconto1}%`)
+} else {
+    console.log(`NON ha diritto allo sconto del ${sconto1}%`)
+}
+
 
