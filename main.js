ba59ecd170636b9654ed1fa0c33d2aacf93e7b3d
karlo-kasi <karlokasi1995@gmail.com>
2025-01-09 18:48:03 +0100

    Add: check up number

--- main.js
+++ main.js
@@ -10,18 +10,24 @@
 1 - Inizializzare / dichiarare le variabili e costanti
 2 - check up se se valori sono numeri 
     utilizzare un metodoto per trasformare le stringhe in numeri parsetInt
+      2.1 - verifica che se sotto i 18 - applicare uno sconto del 20%, sennò niente sconto
+      2.2 - verificare che se sopra i 65 anni - applicare uno sconto del 40%, sennò niente sconto 
+3 - Stampare il risultato su console 
 */
 
-let km = parseInt ( prompt("Quanti chilometri devi fare?", Math.floor(Math.random() * 100) + 20));
-let età = parseInt ( prompt("Quanti anni hai?",  Math.floor(Math.random() * 100) + 10));
+// 1 - Dichiarare i Valori
+let km = prompt("Quanti chilometri devi fare?", Math.floor(Math.random() * 100) + 20);
+let età = prompt("Quanti anni hai?",  Math.floor(Math.random() * 100) + 10);
 const kmInEuro = 0.21;
 
-// 2 - check up se i valori sono numeri
-if( isNaN(età)) {
+console.log (parseInt (km), parseInt (età))
+
+// 2 - Controlla se i valori sono numeri
+if (!isNaN(età) && !isNaN(km)) {
     //se true
-    console.log (`${età} è un numero`)
+    console.log (`${età} e ${km} sono un numero`)
 } else {
     //se foulse
-    console.log (`${età} non è un numero`)
-    età = prompt("Quanti anni hai?")
+    console.log (`${età} e ${km} NON sono un numero`)
 }
+
